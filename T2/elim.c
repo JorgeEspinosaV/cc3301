5f9a137888527d56c5dfea1cb1f46f136c8b47df
jorge <jorge@debian.debian>
2023-09-04 00:13:54 -0300

    ff

--- T2/elim.c
+++ T2/elim.c
@@ -5,9 +5,14 @@
 #include "elim.h"
 
 void eliminar(char *str, char *pat) {
-  ...
+   char *ptr_i = str, *ptr_j = str;
+   size_t len = strlen(str);
+   char temp;
+   while ((ptr_i < len) || (ptr_j < len)) {
+      
+   }
 }
 
 char *eliminados(char *str, char *pat) {
-  ...
+   return pat;
 }
