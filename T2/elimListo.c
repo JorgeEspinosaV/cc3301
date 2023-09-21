12fcc418bcda5fb8057b9c3ceb4a5adb34ce8e21
jorge <jorge@debian.debian>
2023-09-21 19:12:15 -0300

    T2 lista con el zip

--- T2/elimListo.c
+++ T2/elimListo.c
@@ -3,24 +3,12 @@
 #include <string.h>
 #include "elim.h"
 
-int comp(const char *str,const char *pat, int n){
-   while (n--){
-      if(*str=='\0' || *str!=*pat){
-         return 1;
-      }
-      str++;
-      pat++;
-   }
-   return 0;
-}
-
 void eliminar(char *str, char *pat) {
    int len_pat = strlen(pat); // largo del patron
    char *ptr_escritura = str; // ptr que reescribira str sin el pat
-   
    for (char *ptr_lectura = str; *ptr_lectura != '\0';ptr_lectura++){
-      if(comp(ptr_lectura, pat, len_pat)==0) {
-         ptr_lectura += len_pat-1;
+      if(strncmp(ptr_lectura,pat, len_pat)==0) {
+         ptr_lectura += len_pat - 1;
       } else {
          *ptr_escritura = *ptr_lectura;
          ptr_escritura++;
@@ -31,11 +19,11 @@ void eliminar(char *str, char *pat) {
 
 char *eliminados(char *str, char *pat) {
    int len_pat = strlen(pat); // largo del patron
-   int len_str = strlen(str); // largo del str
-   char *ptr_escritura = malloc(len_str+1);// creamos la nueva str
+   char *ptr_escritura = malloc(strlen(str) + 1);// creamos la nueva str
+   strcpy(ptr_escritura, str);
    char *ptr_init = ptr_escritura;
    for (char *ptr_lectura = str; *ptr_lectura != '\0';ptr_lectura++){
-      if(comp(ptr_lectura,pat, len_pat)==0) {
+      if(strncmp(ptr_lectura,pat, len_pat)==0) {
          ptr_lectura += len_pat - 1;
       } else {
          *ptr_escritura = *ptr_lectura;
